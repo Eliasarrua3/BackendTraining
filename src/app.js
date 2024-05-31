71abb6ea5daa83c55f9c3270e0da3bc09a51f0f9
lilfracasado <santiagomkmj@gmail.com>
2024-05-31 19:54:34 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+let tasks = [];
+let helloWorld = "Hello World";
+const server = http.createServer((req, res) => {
+  const {method, url} = req;
+
+  if (url === '/' && method === 'GET'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(helloWorld))
+  }
+  if (url === '/tasks' && method === 'GET'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(tasks))
+  }
+});
+
+const port = 3000;
+server.listen(port, () =>{
+console.log(`Server running at http://localhost:${port}/`)
+});
